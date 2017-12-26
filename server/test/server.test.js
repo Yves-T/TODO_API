736ecfd0469cba974c46c775d322bca816b8193d
Yves Talboom <yves.talboom@gmail.com>
2017-12-26 14:22:38 +0100

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -245,9 +245,28 @@ describe('POST /users/login', () => {
       })
       .expect(400)
       .expect(res => {
-        console.log(res);
         expect(res.headers['x-auth']).to.not.exist();
       })
       .end(done);
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens).to.have.length(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
   });
 });
